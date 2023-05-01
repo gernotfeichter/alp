d48cbf863d1fa134aff217be659c9fe1e112bc18
Gernot Feichter <gernot.feichter@bearingpoint.com>
2023-05-01 14:18:36 +0200

    feat(android): begin adding awesome_notifications (notification shows)

--- android/lib/notifications/notifications.dart
+++ android/lib/notifications/notifications.dart
@@ -1,6 +1,8 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 
+import 'controller.dart';
+
 configureNotifications() {
   AwesomeNotifications().initialize(
     // set the icon to null if you want to use the default app icon
@@ -21,5 +23,28 @@ configureNotifications() {
             channelGroupName: 'Basic group')
       ],
       debug: true
+  );
+  AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+    if (!isAllowed) {
+      // This is just a basic example. For real apps, you must show some
+      // friendly dialog box before call the request method.
+      // This is very important to not harm the user experience
+      AwesomeNotifications().requestPermissionToSendNotifications();
+    }
+  });
+  AwesomeNotifications().setListeners(
+      onActionReceivedMethod:         NotificationController.onActionReceivedMethod,
+      onNotificationCreatedMethod:    NotificationController.onNotificationCreatedMethod,
+      onNotificationDisplayedMethod:  NotificationController.onNotificationDisplayedMethod,
+      onDismissActionReceivedMethod:  NotificationController.onDismissActionReceivedMethod
+  );
+  AwesomeNotifications().createNotification(
+      content: NotificationContent(
+          id: 10,
+          channelKey: 'basic_channel',
+          title: 'Simple Notification',
+          body: 'Simple body',
+          actionType: ActionType.Default,
+      )
   );
 }
