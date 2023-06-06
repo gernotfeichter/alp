b88eddbe1ac842adec032320fa20bda74303c2e9
gernotfeichter <absolutanon@hotmail.com>
2023-06-06 10:33:04 +0200

    fix: navigation bar not going away buy using named routes

--- android/lib/widgets/home.dart
+++ android/lib/widgets/home.dart
@@ -1,26 +1,26 @@
 import 'package:android/widgets/logs/logs.dart';
 import 'package:android/widgets/settings/settings.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-Widget currentWidgetOuter = const Settings();
-final navigationProvider = Provider<Widget>((ref) => currentWidgetOuter);
-
-GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
 class Home extends ConsumerWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    var currentWidget = ref.watch(navigationProvider);
+
     return MaterialApp(
+      initialRoute: "/home",
+      routes: {
+        "/home": (context) => const Settings(),
+        "/settings": (context) => const Settings(),
+        "/logs": (context) => const Logs(),
+      },
       home: Scaffold(
-        key: _scaffoldKey,
         appBar: AppBar(
-          title: Text(currentWidget.toStringShort()),
+          title: Text(Navigator.of(context).toStringShort()),
         ),
-        body: currentWidget,
         drawer: Drawer(
           child: Material(
             child: ListView(
@@ -28,17 +28,13 @@ class Home extends ConsumerWidget {
                 ListTile(
                   title: const Text("Settings"),
                   onTap: () {
-                    currentWidgetOuter = const Settings();
-                    ref.invalidate(navigationProvider);
-                    _scaffoldKey.currentState?.openEndDrawer();
+                    Navigator.pushNamed(context, '/settings');
                   },
                 ),
                 ListTile(
                   title: const Text("Logs"),
                   onTap: () {
-                    currentWidgetOuter = const Logs();
-                    ref.invalidate(navigationProvider);
-                    _scaffoldKey.currentState?.openEndDrawer();
+                    Navigator.pushNamed(context, '/logs');
                   },
                 ),
               ],
