b6783263ed8fb1a5be5d482c2f65dcda74472ddd
gernotfeichter <absolutanon@hotmail.com>
2023-06-10 22:59:16 +0200

    fix: seems I only need a background service

--- android/lib/background_service/background_service.dart
+++ android/lib/background_service/background_service.dart
@@ -6,31 +6,23 @@ import 'dart:ui';
 import 'package:flutter_background_service/flutter_background_service.dart';
 import '../init/init.dart' as init_lib;
 
-import '../notifications/notifications.dart';
-
 Future init() async {
-  DartPluginRegistrant.ensureInitialized();
   final service = FlutterBackgroundService();
   await service.configure(
     androidConfiguration: AndroidConfiguration(
       // this will be executed when app is in foreground or background in separated isolate
-      onStart: foregroundService,
+      onStart: backgroundService,
 
       // auto start service
       autoStart: true,
       autoStartOnBoot: true,
-      isForegroundMode: true,
-
-      foregroundServiceNotificationId: foregroundServiceNotificationId,
-      notificationChannelId: foregroundServiceNotificationChannelKey, // this channel must be created before calling this, see notifications.dart:init
-      initialNotificationTitle: foregroundServiceNotificationMessage,
-      initialNotificationContent: foregroundServiceNotificationContent,
+      isForegroundMode: false,
     ),
     iosConfiguration: IosConfiguration()
   );
   await service.startService();
 }
 
-Future<void> foregroundService(ServiceInstance service) async {
+Future<void> backgroundService(ServiceInstance service) async {
   init_lib.init();
 }
--- android/lib/init/init.dart
+++ android/lib/init/init.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../logging/logging.dart';
 import '../logging/logging.dart' as logging;
-import '../foreground_service/foreground_service.dart' as foreground_service;
+import '../background_service/background_service.dart' as background_service;
 import '../rest_api_server/rest_api_server.dart' as rest_api_server;
 import '../notifications/notifications.dart' as notifications;
 
@@ -14,7 +14,7 @@ Future<void> init() async {
     log.info("initializing");
     WidgetsFlutterBinding.ensureInitialized();
     await notifications.init();
-    await foreground_service.init();
+    await background_service.init();
     await rest_api_server.init();
     log.info("initialized");
     initialized = true;
