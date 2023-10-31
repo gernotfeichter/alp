b865cf0844bb2e74d497dbbc4bec9b167f660019
Gernot Feichter <absolutanon@hotmail.com>
2023-10-31 19:07:28 +0100

    feat: nix package

--- linux/release.sh
+++ linux/release.sh
@@ -7,7 +7,7 @@ new_version="$1"
 # assert correct dir
 [ "$(basename "$(echo "$PWD")")" != 'linux' ] && "ERROR: you need to be in the linux directory to run this command properly!" && exit 1
 # assert correct branch
-[ "$(git rev-parse --abbrev-ref HEAD)" != 'master' ] && "ERROR: you need to be on the master branch to runt his command properly!" && exit 1
+[ "$(git rev-parse --abbrev-ref HEAD)" != 'master' ] && "ERROR: you need to be on the master branch to run this command properly!" && exit 1
 
 # load secrets
 . github-token.sh # contains export GITHUB_TOKEN="<secret_token_here>"
