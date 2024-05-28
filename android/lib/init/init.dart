2e5b8737ad923e329e7472fed203ee77031f3bb7
Gernot Feichter <gernotfeichter@gmail.com>
2024-05-28 22:28:43 +0200

    fix: notification button colors lost

--- android/lib/init/init.dart
+++ android/lib/init/init.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../logging/logging.dart';
 import '../logging/logging.dart' as logging;
-import '../background_service/background_service.dart' as background_service;
 import '../rest_api_server/rest_api_server.dart' as rest_api_server;
 import '../notifications/notifications.dart' as notifications;
 
@@ -11,7 +10,6 @@ Future<void> init() async {
   log.info("initializing");
   WidgetsFlutterBinding.ensureInitialized();
   await notifications.init();
-  await background_service.init();
   await rest_api_server.init();
   log.info("initialized");
 }
--- android/lib/main.dart
+++ android/lib/main.dart
@@ -1,10 +1,34 @@
 import 'package:alp/widgets/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'init/init.dart' as init;
 
-void main() {
+void main() async {
   runApp(const ProviderScope(child: Home()));
-  init.init();
+
+  // init
+  // normally I add everything into separate in lib, but the author explicitly
+  // said it is highly recommended to put this into main
+  // https://pub.dev/packages/flutter_background_service
+  // It's highly recommended to call this method in main() method to ensure the
+  // callback handler updated.
+  final service = FlutterBackgroundService();
+  await service.configure(
+      androidConfiguration: AndroidConfiguration(
+        // this will be executed when app is in foreground or background in separated isolate
+        onStart: backgroundService,
+        // auto start service
+        autoStart: true,
+        autoStartOnBoot: true,
+        isForegroundMode: false,
+      ),
+      iosConfiguration: IosConfiguration()
+  );
+  await service.startService();
 }
 
+@pragma('vm:entry-point')
+Future<void> backgroundService(ServiceInstance service) async {
+  await init.init();
+}
